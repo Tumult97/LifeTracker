c730ceac4cec78891ea4e43d77dff3166a1c7c05
Tristan van der Merwe <tristanvdm87@gmail.com>
2024-07-14 17:17:40 +0200

    Add Github Files

---
+++ .github/pull_request_template.md
@@ -0,0 +1,21 @@
+# Description
+- DESCRIBE_CHANGES_MADE
+
+# Issues
+- LINK_RELEVANT_ISSUES
+
+# Type Of Changes
+- [ ] 🖥️ Feature
+- [ ] 🐛 Bugfix
+- [ ] 🔁 Refactor
+- [ ] 🧹 Cleanup
+- [ ] 🧪 Tests
+- [ ] 😕 Other
+    - Specify type of change if so
+
+# Testing Evidence
+
+| Description | Evidence                                                                          |
+|:------------|:----------------------------------------------------------------------------------|
+| DESCRIPTION | <details><summary>SHORT_SUMMARY</summary> <video src="LINK_TO_VIDEO" /></details> |
+| DESCRIPTION | <details><summary>SHORT_SUMMARY</summary> <img src="LINK_TO_IMG" /></details>     |
---
+++ LifeTracker.Infrastructure/Migrations/20240714124219_Delete_Tables_RemoveGroups-And-UserGroups.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LifeTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Delete_Tables_RemoveGroupsAndUserGroups : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Groups_GroupEntityId",
+                schema: "Users",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "UserGroups",
+                schema: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Groups",
+                schema: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_GroupEntityId",
+                schema: "Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "GroupEntityId",
+                schema: "Users",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GroupEntityId",
+                schema: "Users",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                schema: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedByUserId = table.Column<int>(type: "int", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
+                    ModifiedbyUserId = table.Column<int>(type: "int", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserGroups",
+                schema: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GroupId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGroups", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserGroups_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalSchema: "Users",
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserGroups_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "Users",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_GroupEntityId",
+                schema: "Users",
+                table: "Users",
+                column: "GroupEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGroups_GroupId",
+                schema: "Users",
+                table: "UserGroups",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGroups_UserId",
+                schema: "Users",
+                table: "UserGroups",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Groups_GroupEntityId",
+                schema: "Users",
+                table: "Users",
+                column: "GroupEntityId",
+                principalSchema: "Users",
+                principalTable: "Groups",
+                principalColumn: "Id");
+        }
+    }
+}
